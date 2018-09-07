d615979893077ff2cf9c87e346a7996fa8b8b75d
Josh Carroccia <agr.carroccia@gmail.com>
2018-09-07 15:19:35 -0400

    post controller and crud minus update made

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,36 @@
+class API::PostsController < ApplicationController
+  before_action :set_user, only: [:index, :show, :create, :destroy]
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  def index
+    @posts = @user.posts
+    render json: @posts
+  end
+
+  def show
+    render json: @post
+  end
+    
+  def create
+    @post = @user.posts.create(post_params)
+    render json: @post
+  end
+  
+  def destroy
+    @post.destroy
+    render json: @user.posts
+  end
+  
+  private 
+  def post_params
+    params.require(:post).permit(:body)
+  end
+  
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/create'
+    get 'posts/destroy'
+  end
+  namespace :api do
     resource :session, only: :create
     resources :users do
       resources :posts do
