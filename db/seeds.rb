55210f42fb49236dcbdd3f2de07490cd4d6595fd
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-09-07 14:10:17 -0400

    Added Faker Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+100.times do
+  User.create!(
+    username: Faker::Internet.username,
+    password: 'user'
+    )
+end
+
+
+500.times do
+  Post.create!(
+    user_id: 1 + rand(100),
+    body: Faker::GreekPhilosophers.quote
+    )
+end
+
+
+250.times do
+  Follower.create!(
+    user_id: 1 + rand(100),
+    follower_id: 1 + rand(100) 
+  )
+end
+
+250.times do
+  Favorite.create!(
+    post_id: 1 + rand(500),
+    user_id: 1 + rand(100) 
+  )
+end
